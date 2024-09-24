91214cbf2cd57ad224a9e3c5732e210559909548
Eric Quadros <ericquadros7@gmail.com>
2024-09-23 22:03:51 -0300

    Add migration

---
+++ src/EcommerceOrderManagement.Migrations/Migrations/20240923232917_AddColumnCardPaymentInstallments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceOrderManagement.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnCardPaymentInstallments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Installments",
+                table: "CardPayments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Installments",
+                table: "CardPayments");
+        }
+    }
+}
